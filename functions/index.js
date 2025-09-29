c926f86f548b4a96666ec981ba76169c81de7cc1
yourfreetools <tharsene321@gmail.com>
2025-09-30 00:33:17 +0200

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -894,7 +894,7 @@ function generateIndexHTML(latestPosts, allPostNames) {
                 </a>
                 <ul class="nav-links">
                     <li><a href="reviews">Reviews</a></li>
-                    <li><a href="comparisons">Comparisons</a></li>
+                    <li><a href="https://reviewindex.pages.dev/comparisons">Comparisons</a></li>
                     <li><a href="https://www.youtube.com/channel/UCii1s_g9GPERU4VxXkfsNxw">YouTube</a></li>
                 </ul>
             </nav>
